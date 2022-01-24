207a4d39c13ad21f8ab590d9349abb717bd67ea5
Кожемякин Даниил <megbegb96@yandex.ru>
2022-01-24 10:49:29 +0400

    Авторизация. Дозаливка файла

---
+++ src/main/java/com/testProject/onlineLibrary/service/UserService.java
@@ -0,0 +1,44 @@
+package com.testProject.onlineLibrary.service;
+
+import com.testProject.onlineLibrary.domain.Role;
+import com.testProject.onlineLibrary.domain.User;
+import com.testProject.onlineLibrary.repo.UserRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public boolean addUser(User user){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if(userFromDb != null){
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        userRepo.save(user);
+
+        return true;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByUsername(username);
+    }
+}
